6407d175fc1c918864ceaab3429712b3c4f0a08a
Sean D Kim <seandkim14@gmail.com>
2016-09-01 20:53:06 -0400

    Second Draft

--- README.md
+++ README.md
@@ -1,24 +1,93 @@
+#MLB Robot Jounralism 
 
-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
-    ----------------------------------------------------------------- 
+##About
+This is a program that fetches play-by-play data of a game from MLB.com 
+and analyzes the overall trend of the game. Using the analysis, it detects 
+the highlights of the game and generates a short article (in Korean) on the game chosen.
 
+##Authors
+- Written by Sean D Kim from Carnegie Mellon University 
+- Under the guidance of Professor Joon-Hwan Lee and Dong-Hwan Kim from Seoul National University
 
-Welcome to your Rails project on Cloud9 IDE!
+##Language/Module
+- Ruby on Rails
+- Nokogiri for parsing xml file
 
-To get started, just do the following:
+##Data Source
+- All the game data from MLB.com
+- WE (Win Expectany) and WPA (Win Percentage Added) is calcuated using table 
+    provided by fangraphs.com (?) This table can be found in winexp.csv
 
-1. Run the project with the "Run Project" button in the menu bar on top of the IDE.
-2. Preview your new app by clicking on the URL that appears in the Run panel below (https://mlbdraft1-seandkim14.c9users.io/).
+##Files
+- teamnames.csv: contains abbreviation and the code name of each team
+- winexp.csv: contains WE of a specific situation of a baseball game
 
-Happy coding!
-The Cloud9 IDE team
+##Method
+The program consist of three main parts. 
+1. Data scraping
+  - All the MLB game data are organized at <http://www.mlb.com/gdcross/components/game/mlb/>
+  - To find a specfic game data, navigate through directories in the following order. 
+    - year => month => day => game (starts with gid_...)
+  - Once specific data is found, the following info can be found in the following folders 
+    - players.xml: list of player name and their 6 digit number ID (this number 
+       ID is used in the game data)
+    - game_events.xml: every event in the game is recorded here. 
 
+2. Data Analysis 
+  - From game_events.xml: 
+    - At every <atbat> element, the following data was recorded from the following attributes
+        - Away team score from attribute "away_team_runs"
+        - Home team score from attribute "home_team_runs"
+        - Current Inning from the parent element, <inning>
+        - Ball count from attribute "b"
+        - Strike count from attribute "s"
+        - Out count from attribute "o"
+        - Player present in the first base from attribute "b1"
+        - Player present in the second base from attribute "b2"
+        - Player present in the third base from attribute "b3"
+        - Current pitcher from attribute "pitcher"
+        - Current batter from attribute "batter"
+        - Type of event from attribute "event" (ex. flyout, single)
+        - Description of the event from attribute "des"
+    - At every <action> element, update the necessary situation change using adequate attributes. 
+        - Stolen Base: Attribute "event" indicates which base was stolen. (ex. event="Stolen Base 2b")
+            Update the base situation accordingly.         
+        - Wild Pitch: Attribute "des" identifies the base situation changes. Change accordingly. 
+        - Game Advisory, Manager Review, Pitching Substitution, Offensive Sub, 
+            Defensive Sub: Do nothing. 
+        - Pickoff Error 1B: (Yet Implemented)
+  - Additionally, WE (Win Expectany) and WPA (Win Percentage Added) was calculated using winexp.csv
+    - calculate column and row value
+      - column: (away team points) – (home team points)
+      - row: four digit number generated in the following way
+        - 1st digit: Current inning
+        - 2nd digit: 1 if Top (away team on offense), 2 if Bottom (home team on offense)
+        - 3rd digit: if the player(s) are at the following base(s)...
+          - none => 1
+          - first base => 2
+          - second base=> 3
+          - first & second base => 4
+          - third base=> 5
+          - first & third base => 6
+          - second & third base => 7
+          - all bases => 8
+        - 4th digit: number of outs
+    - the WE is the number found at the column and row calculated above
+    - WPA is calculated by subtracting previous WE from the current WE
+    - WE is closer to 1 if home team is more likely to win, closer to 0 if away team is more likely to win
 
-## Support & Documentation
+3. Generating Article
+  - The highlights of the game is identified by highest change in WE (greatest |WPA|)
+  - The sentence is generated using algorithm developed by the research team of Professor Joon-Hwan Lee from Seoul National University
 
-Visit http://docs.c9.io for support, or to learn more about using Cloud9 IDE. 
-To watch some training videos, visit http://www.youtube.com/user/c9ide
+##Shortcomings
+The followings are the shortcomings that needs to be fixed. 
+- <action> elements 
+    - not all events are being taken care of
+        - Pickoff: base situation update necessary
+        - Substitution: change the player
+        - Search for more events that could happen in a <action> element
+    - when the first element is <action>, the pitcher/batter is empty
+- Database
+    - Current implementation does not make usage of database. Loading players and 
+        storing game data can be done more effectively using database.  
--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -2,13 +2,21 @@ require 'nokogiri'
 require 'open-uri'
 require 'csv'
 
+# EventController
+# takes care of 2 pages: select_game, show (select_date doesn't need functions)
+# passes client's selections (date, game) between pages using parameter
 class EventController < ApplicationController
-  ###############################
-  ###### SELECT_DATE/GAME #######
-  ###############################
-  # takes in a list of directories in a single date
-  # set dirgames_arr with list of game directories
-  # and games_arr with a display version of dirgames_arr
+  ########################################################################### 
+  ############################### SELECT_GAME ###############################
+  ## when a client chooses a date, lookup all the game hosted on that game ##
+  ## and displays a list of those games for the client to choose           ##
+  ###########################################################################
+  
+  # next two functions are helper functions used to create a list of games 
+  # when the client choose a specific date
+  
+  # takes in a list of directories for a single date
+  # filters only the game directories and set the list of them as @game_arr
   def filtergames(texts_arr) 
     @dirgames_arr = []
     @games_arr = []
@@ -22,20 +30,20 @@ class EventController < ApplicationController
     end
   end
   
-  # takes in the name of the directory. 
+  # takes in the name of the directory
   # returns a str for display to client
   # ex) gid_2016_07_10_atlmlb_chamlb_1/ => ATL vs CHA 
   def display(game_dir)
     str = game_dir[15,3].upcase << " vs " << game_dir[22,3].upcase
     return str
   end
-    
 
-  def select_date
-  end
-
+  # takes in nothing, but uses values stored are params 
+  # the params values (year,month,day) are set when client chooses a date
   def select_game
-    @teamnames = loadcsv("teamnames.csv")
+    # list of team names' code name & abbreviation
+    @teamnames = loadcsv("teamnames.csv") 
+    
     year = params["year"]
     month = params["month"]
     day = params["day"]
@@ -51,43 +59,22 @@ class EventController < ApplicationController
     @url = "http://www.mlb.com/gdcross/components/game/mlb/year_#{year}/month_#{month}/day_#{day}/"
 
     games_html = Nokogiri::XML(open(@url))
-    texts_arr = games_html.xpath("//a/text()")
+    texts_arr = games_html.xpath("//a/text()") #list of directories
 
+    # filter out unnecessary (non-game) directories
     filtergames(texts_arr)
   end
   
- 
-  ###############################
-  ############ SHOW #############
-  ###############################
   
-  # takes in a xml doc that has list of players and their ids
-  # returns a hash array where key=id, value=player name
-  def players_xml2ids_hash(players_xml)
-    players_arr = players_xml.css("player")
-    ids_hash = Hash.new
-    
-    players_arr.each do |player| 
-      id = player["id"]
-      first = player["first"]
-      last = player["last"]
-      ids_hash[id] = last
-    end
-    return ids_hash
-  end
+  ########################################################################### 
+  ################################## SHOW ###################################
+  ## when client chooses a specific game, lists the play-by-play data and  ##
+  ## its winning expectancy. Using the change in WE, selects the best play ##
+  ###########################################################################
   
-  
-  def lookup_pitcher(id) 
-    player = @ids_hash[id]
-    return player
-  end
-  
-  def lookup_batter(id) 
-    player = @ids_hash[id]
-    return player
-  end
-  
-  # csv를 array로 불르기
+  # helper function for loading csv file into array
+  # takes in file name
+  # returns an array
   def loadcsv(filename)
     #csv 파일 불러오기
     arr = []
@@ -97,45 +84,68 @@ class EventController < ApplicationController
     return arr
   end
   
-  # takes in a bat event (hash table)
-  # returns [ Win Expectancy of the bat event by looking up at winexp.cvs, 
-  #           a string for debugging (either HOME/AWAY WIN or we_text) ]
-  # set @team_won to "HOME" or "AWAY" once the game is over
-  ### Win Expectancy 구하기
+  # next two are helper functions for player name <=> their six digit number ID
+  # takes in a xml doc that has a list of players and their ids
+  # returns a hash array where key=id, value=player name
+  def players_xml2ids_hash(players_xml)
+    players_arr = players_xml.css("player")
+    ids_hash = Hash.new
+    
+    players_arr.each do |player| 
+      id = player["id"]
+      #first = player["first"]
+      last = player["last"]
+      ids_hash[id] = last
+    end
+    return ids_hash
+  end
+  
+  def lookup_player(id) 
+    player = @ids_hash[id]
+    return player
+  end
+  
+  # takes in a bat event (which is a hash table)
+  # returns Win Expectancy of the bat event by looking up at winexp.cvs
+  # set @team_won to "HOME" or "AWAY" if the game is over
+  # set @we_text as a string for debugging
+  # most of the code is extracted from Dong-Hwan's code
   def calc_we(bat_hash)
     #Score Differences(-15 ~ 15) 계산
     score_diff = (bat_hash["home_team_runs"].to_i) - (bat_hash["away_team_runs"].to_i)
     
     # if game is over (table lookup is not necessary)
     if bat_hash["inning"].to_i >= 9
-      #away_team is winning
+      # away_team is winning
       if score_diff < 0
         if (bat_hash["half"] == "BOT" and bat_hash["o"] == "3")
           @team_won = "AWAY"
-          return [0.0, "AWAY_WIN"]
+          @we_text = "AWAY_WIN"
+          return 0.0 #home team lost for sure
         end
-      #home_team is winning
+      # home_team is winning
       elsif score_diff > 0 
         # away team cannot attack anymore
         if (bat_hash["half"] == "BOT") or ((bat_hash["half"] == "TOP") and (bat_hash["o"]=="3"))
           @team_won = "HOME"
-          return [100.0, "HOME_WIN"]
+          @we_text = "HOME_WIN"
+          return 100.0 #home team won for sure
         end
       end
     end
     
-    #조건1. Innings(1~9)
-    # what if more than 12 inning?
+    # need to generate four-digit for looking up in csv file
+    # 조건1. Innings(1~9)
     we_inning = bat_hash["inning"].to_s
     
-    #조건2. Innings_status(1 or 2)
+    # 조건2. Innings_status(1 or 2)
     if bat_hash["half"] == "TOP"
       we_inning_status = "1"
     else
       we_inning_status = "2"
     end
     
-    #조건3. Basesit :: 1) none, 2) 1st, 3) 2nd, 4) 1st & 2nd, 5) 3rd, 6) 1st & 3rd, 7) 2nd & 3rd, 8) loaded
+    # 조건3. Basesit :: 1) none, 2) 1st, 3) 2nd, 4) 1st & 2nd, 5) 3rd, 6) 1st & 3rd, 7) 2nd & 3rd, 8) loaded
     b1 = bat_hash["b1"]
     b2 = bat_hash["b2"]
     b3 = bat_hash["b3"]
@@ -159,7 +169,7 @@ class EventController < ApplicationController
       we_runners = "9" #should not happen
     end
     
-    #조건4. Outs(0~2)
+    # 조건4. Outs(0~2)
     we_out = bat_hash["o"]
     if bat_hash["o"] == "3"
       # deleted specific case ??
@@ -183,6 +193,7 @@ class EventController < ApplicationController
     
     #조건 1~4를 합쳐서 "InnBaseOut" 문자열 만들기 # why to_s??
     we_text = we_inning + we_inning_status + we_runners + we_out
+    @we_text = we_text #for debugging
     
     #winexp.csv 테이블에서 스코어 차이에 따른 컬럼 위치 구하기.
     we_column = score_diff + 21
@@ -198,50 +209,131 @@ class EventController < ApplicationController
       if @winexp_arr[j][0] == we_text
         winexpcalc = @winexp_arr[j][we_column].to_f
         win_expectancy = (winexpcalc*100).round(1) #in percentage
-        return [win_expectancy, we_text]
+        return win_expectancy
       end
     end
   end ### calc_we 메쏘드 닫기
 
   
-  
   # takes in inning number, xml doc of a single inning, whether TOP or BOT
   # returns an array of "atbat"s with relevent information
-  def bats_xml2bats_arr(i, bats_xml, half)
+  def bats_xml2bats_arr(inn, bats_xml, half)
     #at_bat events in top/bottom of a single inning
     bats_arr = Array.new
     
-    bats_xml.each do |bat_node|
-      #bat_arr = [away_score, home_score, inningnum + top/bottom, 
-      #           ball/strike/out count, pitcher, batter, event, description]
+    (1..(bats_xml.length)).each do |i|
+      bat_node = bats_xml[i-1]
+    
+      #bat_hash = [away_score, home_score, inningnum (as str), half (top/bottom) 
+      #           ball/strike/out count, base situations, pitcher, batter, 
+      #           event (type), description, WE, WPA]
       
-      #single at_bat event
+      #single bat event (either atbat or action)
       bat_hash = Hash.new
+      
+      # distinguish atbat and action node
+      # if action, use previous situation (because data are not recorded)
+      if bat_node["pitcher"].to_i > 0
+        bat_hash["type"] = "atbat"
+      else
+        bat_hash["type"] = "action"
+      end
+    
       bat_hash["away_team_runs"] = bat_node["away_team_runs"] 
       bat_hash["home_team_runs"] = bat_node["home_team_runs"] 
-      bat_hash["inning"] = i.to_s
+      bat_hash["inning"] = inn.to_s
       bat_hash["half"] = half
-      bat_hash["b"] = bat_node["b"]
-      bat_hash["s"] = bat_node["s"]
-      bat_hash["o"] = bat_node["o"]
-      bat_hash["b1"] = bat_node["b1"]
-      bat_hash["b2"] = bat_node["b2"]
-      bat_hash["b3"] = bat_node["b3"]
-      bat_hash["pitcher"] = lookup_pitcher(bat_node["pitcher"])
-      bat_hash["batter"] = lookup_batter(bat_node["batter"])
       bat_hash["event"] = bat_node["event"]
       bat_hash["des"] = bat_node["des"]
-      temp_arr = calc_we(bat_hash)
-      bat_hash["WE"] = temp_arr[0]
-      bat_hash["str"] = temp_arr[1]
-      bat_hash["WPA"] = (bat_hash["WE"] - @prev_WE).round(1)
-      @prev_WE = bat_hash["WE"]
+      
+      if (bat_hash["type"] == "atbat") || (i==1) 
+        bat_hash["b"] = bat_node["b"]
+        bat_hash["s"] = bat_node["s"]
+        bat_hash["o"] = bat_node["o"]
+        bat_hash["b1"] = bat_node["b1"]
+        bat_hash["b2"] = bat_node["b2"]
+        bat_hash["b3"] = bat_node["b3"]
+        bat_hash["pitcher"] = lookup_player(bat_node["pitcher"])
+        bat_hash["batter"] = lookup_player(bat_node["batter"])
+        
+        bat_hash["WE"] = calc_we(bat_hash) #float
+        bat_hash["str"] = @we_text
+        bat_hash["WPA"] = (bat_hash["WE"] - @prev_bat["WE"]).round(1) #float
+        @prev_bat = bat_hash
+      
+      #if action and first event of the inning (no previous bat event)
+      elsif i==1  
+        bat_hash["b"] = "0"
+        bat_hash["s"] = "0"
+        bat_hash["o"] = "0"
+        bat_hash["b1"] = ""
+        bat_hash["b2"] = ""
+        bat_hash["b3"] = ""
+        
+      #if action, then copy previous bat event
+      else #elsif bat_hash["type"] == "action" 
+        bat_prev = bats_arr[i-2]
+        bat_hash["b"] = bat_prev["b"]
+        bat_hash["s"] = bat_prev["s"]
+        bat_hash["o"] = bat_prev["o"]
+        bat_hash["b1"] = bat_prev["b1"]
+        bat_hash["b2"] = bat_prev["b2"]
+        bat_hash["b3"] = bat_prev["b3"]
+        bat_hash["pitcher"] = bat_prev["pitcher"]
+        bat_hash["batter"] = bat_prev["pitcher"]
+        
+        bat_hash["str"] = "ACTION"
+        
+        #take care of stolen base, wild pitch, etc
+        bat_hash = clean_action(bat_hash)
+        
+        bat_hash["WE"] = calc_we(bat_hash) #float
+        bat_hash["WPA"] = (bat_hash["WE"] - @prev_bat["WE"]).round(1) #float
+        @prev_bat = bat_hash
+      end
+
 
       bats_arr << bat_hash
     end
     return bats_arr
   end
   
+  # takes in bat hash
+  # returns a bat hash with necessary change in base situation
+  def clean_action(bat_hash)
+    cleaned_bat_hash = bat_hash
+    if bat_hash["event"][0,11] == "Stolen Base"
+      if bat_hash["event"][12].to_i == 2
+        cleaned_bat_hash["b2"] = bat_hash["b1"]
+        cleaned_bat_hash["b1"] = ""
+      elsif bat_hash["event"][12].to_i == 3
+        cleaned_bat_hash["b3"] = bat_hash["b2"]
+        cleaned_bat_hash["b2"] = ""
+      #elsif bat_hash["event"][12].to_i == 4??
+      end
+    elsif bat_hash["event"] == "Wild Pitch" || bat_hash["event"] == "Passed Ball"
+      #for debugging
+      cleaned_bat_hash["str"] = bat_hash["event"]
+      
+      #clear out the bases
+      cleaned_bat_hash["b1"] = ""
+      cleaned_bat_hash["b2"] = ""
+      cleaned_bat_hash["b3"] = ""
+      
+      #update base situation using description
+      des = bat_hash["des"]
+      if des.index("to 1st") != nil
+        cleaned_bat_hash["b1"] = "1" #change to player name
+      end
+      if des.index("to 2nd") != nil
+        cleaned_bat_hash["b2"] = "1" #change to player name
+      end
+      if des.index("to 3rd") != nil
+        cleaned_bat_hash["b3"] = "1" #change to player name
+      end
+    end
+    return cleaned_bat_hash
+  end
   
   # takes in xml file of a game 
   # returns game info in a nested array of the following structure:
@@ -250,27 +342,37 @@ class EventController < ApplicationController
   # also set the best play from the team won as @bestBat
   def game_xml2game_table(game_xml)
     game_table = Array.new #array of bat_arr
-    innings_arr = game_xml.css("inning")
+    innings_xml_arr = game_xml.css("inning")
     
     #for calculating WPA
-    @prev_WE = 50.0
+    @prev_bat = Hash.new
+    @prev_bat["WE"] = 50.0 #equal probability at first
     
-    (1..(innings_arr.length)).each do |i|
-      inning = innings_arr[i-1]
+    (1..(innings_xml_arr.length)).each do |i|
+      inning = innings_xml_arr[i-1]
       #top of a inning
       bats_str = inning.css("top")
-      bats_xml = bats_str.css("atbat")
+      
+      #keeps the order of atbat and action
+      bats_xml = bats_str.search('atbat, action')
+      bats_xml.map(&:to_xml)
+      
       bats_arr = bats_xml2bats_arr(i, bats_xml, "TOP")
       game_table << bats_arr
 
       #bottom of a inning
       bats_str = inning.css("bottom")
-      bats_xml = bats_str.css("atbat")
+      
+      #keeps the order of atbat and action
+      bats_xml = bats_str.search('atbat, action')
+      bats_xml.map(&:to_xml)
+      
       bats_arr = bats_xml2bats_arr(i, bats_xml, "BOT")
       game_table << bats_arr
     end 
     
     @bestBat = bestBat(game_table)
+    
     
     return game_table
   end
@@ -283,11 +385,11 @@ class EventController < ApplicationController
     if @team_won == "HOME"
       scalar = 1
     else
-      scalar = -1
+      scalar = -1 #get the most negative WPA
     end
     
     bestBat = Hash.new
-    bestWPA = -100.1
+    bestWPA = -100.1 #lower than lowest WPA
     
     game_table.each do |inning|
       inning.each do |bat_hash|
